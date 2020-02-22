06a5ccb0795d82dc636cb35c4e3dbfe0137266a8
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-02-22 18:52:59 +0100

    Camera Behaviour and Player Behaviour started

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject target = null;
+    public float MinHeight = 4f;
+    public float HeightFactor = 1.0f;
+
+    private float Height;
+    private Rigidbody shipRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipRigidBody = target.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.transform.position.x,
+                                             target.transform.position.y + Height,
+                                             target.transform.position.z);
+
+            Height = MinHeight * (1 + shipRigidBody.velocity.magnitude / HeightFactor);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float TranslateSpeed = 1f;
+    public float RotateSpeed = 1f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * TranslateSpeed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * TranslateSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.up * Time.deltaTime * RotateSpeed);
+        }
+
+    }
+}
